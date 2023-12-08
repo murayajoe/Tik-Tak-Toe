a7f8515d30c5df1967feadd4bc7c7a1985567e92
Murayajoe <joemuraya90@gmail.com>
2023-12-08 23:01:04 +0300

    last

--- tictactoe basic.py
+++ tictactoe basic.py
@@ -22,6 +22,40 @@ def playerInput(board):
         board[inp-1] = currentPlayer
     else: 
         print ("Sorry Player Already in spot")
+
+
+
+ #check for win or tie 
+    def checkHorizontal(board): 
+        global winner
+        if board[0] == board[1] == board[2] and board[0]!= "-":
+            winner = board[0]
+            return True
+        elif board[3] == board[4] == board[5] and board[3]!= "-":
+            winner = board[3]
+            return True
+        elif board[6] == board[7] == board[8] and board[6]!= "-":
+            winner = board[6]
+            return True
         
+
+        def checkRow(board):
+            global winner
+            if board[0] == board[3] == board[6] and board[0]!= "-":
+                winner = board[0]
+                return True
+            elif board[1] == board[4] == board[7] and board[1]!= "-":
+                winner = board[1]
+                return True
+            elif board[2] == board[5] == board[8] and board[2]!= "-":
         
+
+        def checkDiagonal(board)
+            global winner
+            if board[0] == board[4] == board[8] and board[0]!= "-":
+                winner = board[0]
+                return True
+            elif board[2] == board[4] == board[6] and board[2]!= "-":
+                winner = board[2]
+                return True
         
